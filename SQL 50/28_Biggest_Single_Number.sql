8393a7c8d6637741e797016b3eba3cc2bdda6beb
Your Name <tanyachaurasia1403@gmail.com>
2026-07-23 13:49:07 +0530

    2

---
+++ SQL 50/28_Biggest_Single_Number.sql
@@ -0,0 +1,23 @@
+/*
+Table: MyNumbers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+Each row of this table contains an integer.
+ 
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+Find the largest single number. If there is no single number, report null.
+*/
+
+SELECT max(num) AS num
+FROM 
+    (SELECT num 
+    FROM MyNumbers 
+    GROUP BY num 
+    HAVING COUNT(*)=1) T;
